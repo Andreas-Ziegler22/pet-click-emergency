827fa4035835f5af62675a25e85fd11623720545
MrBurp <andreass.ziegler@gmail.com>
2022-08-20 12:34:40 +0200

    finding my location

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const btn = document.getElementById("btn");
+const local = document.getElementById("myLocal");
 
-btn.addEventListener("click", call);
+btn.addEventListener("click", call, getLocation);
 
 function call() {
   document.getElementById("text").innerHTML = `calling...`;
+}
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    location.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  local.innerHTML = `Latitude: ${position.coords.latitude} Longitude: ${position.coords.longitude} `;
 }
